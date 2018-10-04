437578082d5505b2503ddeb547af7882457ca631
vikas <nilesh.s@panaceatek.com>
2018-10-04 18:58:05 +0530

    all

---
+++ polls/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from .models import Question
+from django.http import Http404
+from django.template import loader
+from django.shortcuts import render
+
+def index(request):
+    questions = Question.objects.all();
+#    template = loader.get_template('polls/index.html')
+    context = {
+        'questions': questions,
+    }
+    return render(request,'polls/index.html',context)
+    #return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist") 
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
